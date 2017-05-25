3660688b8487de240abccb75f70aa60abdfbb21e
glemma32 <genet.woldeyes@gmail.com>
2017-05-25 14:33:44 -0400

    first commit

---
+++ EscapeSequence.java
@@ -0,0 +1,16 @@
+
+public class EscapeSequence {
+	public static void main(String[] args) {
+		System.out.println("  ***  ");
+		System.out.println("       ");
+		System.out.println(" *   * ");
+		System.out.println("       ");
+		System.out.println("*     *");
+		System.out.println("       ");
+		System.out.println("*     *");
+		System.out.println("       ");
+		System.out.println(" *   * ");
+		System.out.println("       ");
+		System.out.println("  ***  ");
+}
+}
